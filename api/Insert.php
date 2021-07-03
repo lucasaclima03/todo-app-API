56b21109f422a54ecfba5a5febb8e4207a9afac2
lucasaclima03 <79098716+lucasaclima03@users.noreply.github.com>
2021-07-03 18:17:48 -0300

    correção da rota para adicionar notas

--- api/Insert.php
+++ api/Insert.php
@@ -5,26 +5,28 @@ $method = strtolower($_SERVER['REQUEST_METHOD']);
 
 if ($method === 'post') {
 
-    $id = filter_input(INPUT_POST, 'id');
     $title = filter_input(INPUT_POST, 'title');
-    $created_at = filter_input(INPUT_POST, 'created_at');
-    $updated_at = filter_input(INPUT_POST, 'updated_at');
-    $is_active = filter_input(INPUT_POST, 'is_active');
+    
 
-    $sql = $pdo->prepare("INSERT INTO tasks (id, title, created_at, updated_at, is_active)  VALUES
-                         (:id, :title, :created_at, :updated_at, :is_active)");
-    $sql->bindValue(':id', $id);
+    $sql = $pdo->prepare("INSERT INTO tasks (title, created_at, updated_at, is_active)  VALUES
+                         (:title, :created_at, :updated_at, :is_active)");
+    
+    $now = date('Y-m-d H:i:s');
+    $is_active = 0;
+
     $sql->bindValue(':title', $title);
-    $sql->bindValue(':created_at', $created_at);
-    $sql->bindValue(':updated_at', $updated_at);
+    $sql->bindValue(':created_at', $now);
+    $sql->bindValue(':updated_at', $now);
     $sql->bindValue(':is_active', $is_active);
     $sql->execute();
+
+    $id = $pdo->lastInsertId();
 
     $array['result'] = [
         'id' => $id,
         'title' => $title,
-        'created_at' => $created_at,
-        'updated_at' => $updated_at,
+        'created_at' => $now,
+        'updated_at' => $now,
         'is_active' => $is_active
     ];
 
